75113ffd0ddea7b896d0bc4bb3aeea24dd8afe5d
Geoffrey Bascour <geoffreybascour93@gmail.comm>
2019-05-09 10:43:34 +0200

    creation du student

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Vignette from './Components/Vignette';
+import Student from './Components/Student'
 
 function App() {
   return (
     <div className="App">
-    
+      <Student />
       <Vignette />
+      
     </div>
   );
 }
