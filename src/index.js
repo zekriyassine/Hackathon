18113a30e71fa32cd22c8e85665991323c67d1d4
rob17833 <janssensrobert@hotmail.com>
2019-05-09 17:50:44 +0200

    portal modif

--- src/index.js
+++ src/index.js
@@ -5,33 +5,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 69d0225157d1ca0412d291b2e0445cd28ef0c014
->>>>>>> 4c4e6a001158612079b64eb7b27e58caf9000f5f
 ReactDOM.render(
     <BrowserRouter>   
         <App />
     </BrowserRouter>
     , document.getElementById('root'));
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 4c4e6a001158612079b64eb7b27e58caf9000f5f
 
 
 
 
-<<<<<<< HEAD
-=======
 
-=======
->>>>>>> 69d0225157d1ca0412d291b2e0445cd28ef0c014
->>>>>>> 4c4e6a001158612079b64eb7b27e58caf9000f5f
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
