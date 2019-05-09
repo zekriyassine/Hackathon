1fcd170ae5600ce82e2c27a774833cf2c0e4e586
Geoffrey Bascour <geoffreybascour93@gmail.comm>
2019-05-09 20:31:26 +0200

    police

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.css'
 import { Switch, Route } from 'react-router-dom';
 import Header from './Components/Header/index';
 import Vignette from './Components/Vignette';
 import Student from './Components/Student';
 import Terminal from './Components/Terminal';
 import Form from './Components/Form/index'
+
 
 function App() {
   return (
