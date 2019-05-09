c8390b873458c371b23ccce8c969e24ef7998a28
rob17833 <janssensrobert@hotmail.com>
2019-05-10 01:11:33 +0200

    css portal

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Vignette from './Components/Vignette';
 import Student from './Components/Student/index';
-import Form from './Components/Form/index'
+import Form from './Components/Form/index';
+import Portal from './Components/Portal/index';
 
 function App() {
   return (
     <div className="App">
+      <Portal />
       <Switch>
         <Route exact path="/" />
         <Route path="/home" component={Vignette} />
